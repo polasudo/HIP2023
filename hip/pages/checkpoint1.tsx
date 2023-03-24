286f045dfa8d7e9965094f5e8e8b9255df150a26
polasudo <50864147+polasudo@users.noreply.github.com>
2023-03-24 17:48:06 +0100

    done with the checkpoint

--- hip/pages/checkpoint1.tsx
+++ hip/pages/checkpoint1.tsx
@@ -4,17 +4,24 @@ import Image from 'next/image';
 function checkpoint1() {
     return (
         <>
-            <div className='bg-black h-screen text-white text-center flex'>
-                
+            <div className="text-white h-screen bg-black justify-center grid justify-items-center">
+                <div className='justify-center grid justify-items-center mt-20'>
+                <div>
+                <h1 className="m-auto font-waterfall text-3xl text-purple-600 font-bold ">
+                    Checkpoint 1
+                </h1>
+                </div>
+                <div>
                 <Image
                     src="/logo.svg"
                     width={100}
                     height={100}
                 />
-                <h1 className="m-auto font-waterfall text-3xl text-white font-bold">
-                    Checkpoint 1
-                </h1>
+                </div>
+                </div>
             </div>
+
+
         </>
     )
 }
